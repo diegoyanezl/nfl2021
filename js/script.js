e069f1292cc8533bcb25c016ccee737a0ac16b97
diegoyl <dyanez@mit.edu>
2021-01-08 15:42:12 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-var rounds_so_far = 2; // after wc-1, after div-2, after conf-3, after sb-4
-=======
 var rounds_so_far = 0; // after wc-1, after div-2, after conf-3, after sb-4, FOR POINT KEEPING
 var ALIVEtms = [ // DELETE TEAM WHEN THEY LOSE, FOR BLOCK UPDATING
 	'GB', 'NO', 'CHI', 'SEA', 'LAR', 'WAS', 'TB',
 	'KC', 'PIT', 'CLE', 'BUF', 'IND', 'TEN', 'BAL'
 ];
->>>>>>> Stashed changes
 var REAL = [
 	[ // DIVISIONAL TEAMS
 		'nfcWorst', 
